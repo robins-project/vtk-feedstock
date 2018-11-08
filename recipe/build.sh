244765f01b6b478cbc24061663e6b5c5268f5d39
v-turin <vladimir.turin@opencascade.com>
2018-11-08 15:03:11 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,13 +5,13 @@ cd build
 
 BUILD_CONFIG=Release
 
-export CFLAGS="$CFLAGS -idirafter /usr/include -lm"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
+
+export CFLAGS="$CFLAGS -idirafter /usr/include"
 export CXXFLAGS="$CXXFLAGS -idirafter /usr/include"
+export LDFLAGS="$LDFLAGS -L/usr/lib/x86_64-linux-gnu"
 
 # now we can start configuring
-    # -DVTK_USE_SYSTEM_HDF5:BOOL=ON \
-    # -DVTK_EXTERNAL_HDF5_IS_SHARED:BOOL=ON \
-    # -DVTK_USE_SYSTEM_LZ4:BOOL=OFF \
 cmake -G "Ninja" \
     -Wno-dev \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
@@ -26,7 +26,7 @@ cmake -G "Ninja" \
     -DModule_vtkPythonInterpreter:BOOL=OFF \
     -DModule_vtkRenderingMatplotlib=OFF \
     -DVTK_HAS_FEENABLEEXCEPT:BOOL=OFF \
-    -DVTK_RENDERING_BACKEND=OpenGL \
+    -DVTK_RENDERING_BACKEND=OpenGL2 \
     -DVTK_USE_SYSTEM_ZLIB:BOOL=ON \
     -DVTK_USE_SYSTEM_FREETYPE:BOOL=ON \
     -DVTK_USE_SYSTEM_LIBXML2:BOOL=ON \
@@ -38,30 +38,10 @@ cmake -G "Ninja" \
     -DVTK_SMP_IMPLEMENTATION_TYPE:STRING=TBB \
     -DVTK_USE_SYSTEM_NETCDF:BOOL=OFF \
     -DVTK_USE_SYSTEM_OGGTHEORA:BOOL=OFF \
+    -DCMAKE_LIBRARY_ARCHITECTURE=x86_64-linux-gnu \
+    -DOpenGL_GL_PREFERENCE=GLVND \
     -DVTK_USE_X:BOOL=ON \
-    -DOPENGL_egl_LIBRARY:FILEPATH=/usr/lib/x86_64-linux-gnu/libEGL.so \
-    -DOPENGL_glu_LIBRARY:FILEPATH=/usr/lib/x86_64-linux-gnu/libGLU.so \
-    -DOPENGL_glx_LIBRARY:FILEPATH=/usr/lib/x86_64-linux-gnu/libGLX.so \
-    -DOPENGL_opengl_LIBRARY:FILEPATH=/usr/lib/x86_64-linux-gnu/libOpenGL.so \
-    -DOPENGL_gl_LIBRARY:FILEPATH=/usr/lib/x86_64-linux-gnu/libGL.so \
-    -DOpenGL_GL_PREFERENCE:STRING=GLVND \
-    -DX11_X11_LIB:FILEPATH=/usr/lib/x86_64-linux-gnu/libX11.so \
-    -DX11_Xext_LIB:FILEPATH=/usr/lib/x86_64-linux-gnu/libXext.so \
-    -DX11_Xt_LIB:FILEPATH=/usr/lib/x86_64-linux-gnu/libXt.so \
     ..
 
 # compile & install!
 ninja install
-
-# The egg-info file is necessary because some packages,
-# like mayavi, have a __requires__ in their __init__.py,
-# which means pkg_resources needs to be able to find vtk.
-# See https://setuptools.readthedocs.io/en/latest/pkg_resources.html#workingset-objects
-
-# cat > $SP_DIR/vtk-$PKG_VERSION.egg-info <<FAKE_EGG
-# Metadata-Version: 2.1
-# Name: vtk
-# Version: $PKG_VERSION
-# Summary: VTK is an open-source toolkit for 3D computer graphics, image processing, and visualization
-# Platform: UNKNOWN
-# FAKE_EGG
